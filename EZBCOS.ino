339e1eb11c51e1bb90d717178f22c8b3dd0006aa
PQCraft <58464017+PQCraft@users.noreply.github.com>
2020-11-01 17:33:17 -0500

    Add files via upload

--- EZBCOS.ino
+++ EZBCOS.ino
@@ -1,25 +1,40 @@
 #include <Arduino.h>
 #include <FS.h>
 #include <LITTLEFS.h>
+#include <esp_int_wdt.h>
+#include <esp_task_wdt.h>
+#include <esp32-hal-cpu.h>
+#include <stdlib.h>
 //#include <LittleFS.h>
-struct FSInfo {
+/*struct FSInfo {
     size_t totalBytes;
     size_t usedBytes;
     size_t blockSize;
     size_t pageSize;
     size_t maxOpenFiles;
     size_t maxPathLength;
-};
-#define  fs LITTLEFS
-#include <stdlib.h>
-#define ver "0.0.0.2"
+};*/
+#define fs LITTLEFS
+#define ver "0.0.1.0"
 #define rev "Alpha"
 byte ufgc = 7;
 byte ubgc = 0;
-FILE progFile[16];
-FILE userFile[64];
-byte progMem[262144];
-
+FILE progFile;
+FILE userFile[16];
+long progMemSize = 262144;
+unsigned int progMemChunkSize = 16384;
+byte *progMem;
+//byte progMem0[65536];
+//long progMemSize = sizeof(progMem);
+unsigned long progAddtPtr[256];
+uint8_t  mathU08Reg = 0;
+int8_t   mathS08Reg = 0;
+uint16_t mathU16Reg = 0;
+int16_t  mathS16Reg = 0;
+uint32_t mathU32Reg = 0;
+int32_t  mathS32Reg = 0;
+uint64_t mathU64Reg = 0;
+int64_t  mathS64Reg = 0;
 void setup() {
   Serial.begin(115200);
   cls();
@@ -29,26 +44,50 @@ void setup() {
   //Serial.print(" r");
   //Serial.println(rev);
   delay(3000);
-  FSInfo fs_info;
+  //FSInfo fs_info;
   color(15, 4);
   cls();
   drwlogo();
   statBar("PQWare EZBCOS v" + String(ver) + " r" + String(rev), 0);
+  beep(900, 150);
+  delay(100);
+  beep(900, 150);
   delay(2000);
   color(7, 0);
   cls();
   locate(0, 0);
-  Serial.println("Initalizing filesystem (LittleFS)...");
+  Serial.println("PQWare EZBCOS v" + String(ver) + " r" + String(rev));
+  Serial.println("Initializing memory...");
+  progMem = (byte *) malloc(progMemSize);
+  Serial.print("Allocating memory in ");
+  Serial.print(progMemChunkSize);
+  Serial.println(" byte chunks...");
+  recheckmem:
+  if (progMem == NULL) {
+    progMemSize = progMemSize - progMemChunkSize;
+    if (progMemSize < 1) {
+      Serial.println("Memory allocation failed.");
+      hang();
+    }
+    progMem = (byte *) realloc(progMem, progMemSize);
+    goto recheckmem;
+  }
+  for (long i = 0; i < progMemSize; i++) {
+    progMem[i] = 0;
+  }
+  Serial.print("Program memory size: "); Serial.println(progMemSize);
+  Serial.println("Initalizing filesystem...");
   bool fsFormatted = false;
   recheckfs:
   if (fs.begin()) {
     unsigned long fsTotalBytes = LITTLEFS.totalBytes();
     unsigned long fsUsedBytes = LITTLEFS.usedBytes();
     unsigned long fsFreeBytes = fsTotalBytes - fsUsedBytes;
+    String fsVolLabel = "";
     Serial.println("Mounted filesystem.");
-    Serial.print(char(195)); Serial.println(" Total space: " + String(fsTotalBytes, DEC) + "B");
-    Serial.print(char(195)); Serial.println(" Used space: " + String(fsUsedBytes, DEC) + "B");
-    Serial.print(char(192)); Serial.println(" Free space: " + String(fsFreeBytes, DEC) + "B");
+    Serial.print(char(195)); Serial.println(" Total space: " + String(fsTotalBytes / 1024, DEC) + "KB");
+    Serial.print(char(195)); Serial.println(" Used space: " + String(fsUsedBytes / 1024, DEC) + "KB");
+    Serial.print(char(192)); Serial.println(" Free space: " + String(fsFreeBytes / 1024, DEC) + "KB");
   } else {
     Serial.println("Filesystem mount failed.");
     if (!fsFormatted) {
@@ -110,6 +149,19 @@ int runProg(String progFileName, String progArgs) {
 
 void cls() {
   Serial.print("\u001b[2J\u001b[H");
+}
+
+void beep(unsigned int freq, unsigned int dur) {
+  unsigned long tmpMillis = millis();
+  freq *= 2.05;
+  while (millis() - tmpMillis < dur) {
+    delayMicroseconds(1000000 / freq);
+    dacWrite(25, 255);
+    dacWrite(26, 255);
+    delayMicroseconds(1000000 / freq);
+    dacWrite(25, 0);
+    dacWrite(26, 0);
+  }
 }
 
 void statBar(String statText, byte statType) {
@@ -201,14 +253,13 @@ void drwlogo() {
 
 void hang() {
   while (true) {
-    delay(1000);
-    randomSeed(micros() + millis());
+    randomSeed( micros() + millis());
     long temp1 = random(0, 65536) + random(0, 65536);
     long temp2 = random(0, 65536) - random(0, 65536);
     long temp3 = random(0, 65536) * random(0, 65536);
     long temp4 = random(0, 65536) / random(0, 65536);
     long temp5 = temp1 + temp2 + temp3 + temp4;
     char tempc = char(temp5);
-    locate(79, 24); if (tempc >= ' ' && tempc <= 'ÿ' && tempc != '\x7F') {Serial.print(tempc);}
+    //locate(79, 24); if (tempc >= ' ' && tempc <= 'ÿ' && tempc != '\x7F') {Serial.print(tempc);}
   }
 }
